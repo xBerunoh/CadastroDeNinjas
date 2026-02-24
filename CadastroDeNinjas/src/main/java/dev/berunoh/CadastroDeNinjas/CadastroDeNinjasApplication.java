a32917675f390ae912e568e3fd8fcc813a2f9bf4
Ti Difelicia <brunoeduardo92093676@gmail.com>
2026-02-24 16:02:07 -0400

    first commit

---
+++ CadastroDeNinjas/src/main/java/dev/berunoh/CadastroDeNinjas/CadastroDeNinjasApplication.java
@@ -0,0 +1,13 @@
+package dev.berunoh.CadastroDeNinjas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeNinjasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeNinjasApplication.class, args);
+	}
+
+}
---
+++ CadastroDeNinjas/src/test/java/dev/berunoh/CadastroDeNinjas/CadastroDeNinjasApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.berunoh.CadastroDeNinjas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeNinjasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
