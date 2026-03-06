e91a49657ac7345fe09e202960983695df7c47e7
Ti Difelicia <brunoeduardo92093676@gmail.com>
2026-03-06 13:51:34 -0400

    feat: Adicionado Missoes Service regras de : PUT,DELETE,POST,GET

--- CadastroDeNinjas/src/main/java/dev/berunoh/CadastroDeNinjas/Missoes/MissoesService.java
+++ CadastroDeNinjas/src/main/java/dev/berunoh/CadastroDeNinjas/Missoes/MissoesService.java
@@ -1,4 +1,57 @@
 package dev.berunoh.CadastroDeNinjas.Missoes;
 
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class MissoesService {
+    private final MissoesRepository missoesRepository;
+
+    public MissoesService(MissoesRepository missoesRepository) {
+        this.missoesRepository = missoesRepository;
+    }
+
+    public List<MissoesModel> listarMissoes(){
+    return missoesRepository.findAll();
+
+    }
+
+    public MissoesModel listarMissoesPorId(Long id){
+       Optional<MissoesModel> missoesPorId = missoesRepository.findById(id);
+        return missoesPorId.orElse(null);
+
+
+
+    }
+
+
+
+    public MissoesModel criarMissoes(MissoesModel missoes){
+        return missoesRepository.save(missoes);
+
+    }
+
+
+
+
+    public void deleterMissoes(Long id){
+        missoesRepository.deleteById(id);
+
+
+    }
+
+    public MissoesModel atualizarMissoes(Long id, MissoesModel missoesAtualizada){
+        if(missoesRepository.existsById(id)){
+            missoesAtualizada.setId(id);
+            return missoesRepository.save(missoesAtualizada);
+        }
+        return null;
+    }
+
+
+
+
+
 }
--- CadastroDeNinjas/src/main/java/dev/berunoh/CadastroDeNinjas/Ninjas/NinjaControllerUi.java
+++ CadastroDeNinjas/src/main/java/dev/berunoh/CadastroDeNinjas/Ninjas/NinjaControllerUi.java
@@ -1,4 +1,32 @@
 package dev.berunoh.CadastroDeNinjas.Ninjas;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/ninjas/ui")
 public class NinjaControllerUi {
+
+    private final NinjaService ninjaService;
+
+    public NinjaControllerUi(NinjaService ninjaService) {
+        this.ninjaService = ninjaService;
+    }
+
+
+    @GetMapping("/listar")
+    public String listarNinjas(Model model){
+        List<NinjaDTO> ninjas = ninjaService.listarNinjas();
+        model.addAttribute("ninjas",ninjas);
+        return "Ninjas listados";
+    }
+
+
+
+
 }
